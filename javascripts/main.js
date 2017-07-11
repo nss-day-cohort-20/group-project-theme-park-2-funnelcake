dc041bcb42d02629b088801782c64aea2d659256
Hayley Landsberg <hayleyheath@Hayleys-MacBook-Air.local>
2017-07-11 14:54:22 -0500

    added event listeners for search textbox and enter button. Added id's in HTML

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,23 +3,37 @@
 let $ = require('jquery');
 let fb = require('./fetch-fb');
 
-  // fb.getAreas()
-  // .then((attrData) => {
-// })
+  fb.getAreas()
+  .then((attrData) => {
+});
 
-function sortData(attrData) {
-  var attrMap = attrData.map(function(prop) {
-    if (prop = ) {
+// function sortData(attrData) {
+//   var attrMap = attrData.map(function(prop) {
+//     if (prop = ) {
 
-    }
-    console.log("attrMap", attrMap);
-    return prop ;
-  });
-  // .filter();
+//     }
+//     console.log("attrMap", attrMap);
+//     return prop ;
+//   });
+//   // .filter();
  
+//   }
+
+//   // requirement 3a for associating areas in their attractions
+//   // plan to sort based on id
+
+let textInput = document.getElementById('textInput');
+let submitBtn = document.getElementById('submitBtn');
+
+textInput.addEventListener('keypress', function() {
+  if (textInput.value !== '' && event.key === 'Enter') {
+    console.log("I worked when you pressed enter!");
   }
+});
 
-  // requirement 3a for associating areas in their attractions
-  // plan to sort based on id
+submitBtn.addEventListener("click", function() {
+  console.log("I worked when you clicked the button!");
+});
 
-module.exports = {sortData};
+
+//module.exports = {sortData};
