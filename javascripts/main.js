aed7bb254cc52de8b51450ed14adfca11e9215fb
Adobe-Android <tngamerboy@gmail.com>
2017-07-12 14:35:39 -0500

    Linked files and made material style show. Still lacking functionality

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,7 +2,9 @@
 
 let $ = require('jquery');
 let fb = require('./fetch-fb');
-let themepark = {fbData: require('./fetch-fb')};
+let themepark = {
+    fbData: require('./fetch-fb')
+};
 let attractions = [];
 let areas = [];
 let types = [];
@@ -11,46 +13,76 @@ let textInput = document.getElementById('textInput');
 let submitBtn = document.getElementById('submitBtn');
 
 textInput.addEventListener('keypress', function() {
-  if (textInput.value !== '' && event.key === 'Enter') {
-    console.log("I worked when you pressed enter!");
-  }
+    if (textInput.value !== '' && event.key === 'Enter') {
+        console.log("I worked when you pressed enter!");
+    }
 });
 
 submitBtn.addEventListener("click", function() {
-  console.log("I worked when you clicked the button!");
+    console.log("I worked when you clicked the button!");
 });
 
 //selecting the area boxes
 $(".area").click(function() {
-	$(this).toggleClass("highlight");
+    $(this).toggleClass("highlight");
 });
 
+
+// Bootstap datetimepicker
+$('#time').bootstrapMaterialDatePicker({date: false, shortTime: false, format: 'HH:mm'});
+$.material.init();
+// 
+// 
+// 
+// 
+// $(document).ready(function() {
+//   $('#time').bootstrapMaterialDatePicker({
+//     date: false,
+//     shortTime: false,
+//     format: 'HH:mm'});
+//   $.material.init();
+// }); 
+//     $(function() {
+//         $('#datetimepicker6').datetimepicker();
+//         $('#datetimepicker7').datetimepicker({
+//             useCurrent: false //Important! See issue #1075
+//         });
+//         $("#datetimepicker6").on("dp.change", function(e) {
+//             $('#datetimepicker7').data("DateTimePicker").minDate(e.date);
+//         });
+//         $("#datetimepicker7").on("dp.change", function(e) {
+//             $('#datetimepicker6').data("DateTimePicker").maxDate(e.date);
+//         });
+//     });
+    // $.material.init();
+// });
+
 themepark.fbData.getAttr()
-.then((attrData) => {
-    attractions = attrData;
-    return themepark.fbData.getAreas();
+    .then((attrData) => {
+        attractions = attrData;
+        return themepark.fbData.getAreas();
     })
-.then((areaData) => {
-    areas = areaData;
-    attractions.forEach(function (){
-      for(let i = 0; i < attractions.length; i++) {
-        let myArea = areas.filter((area) => {
-          return attractions[i].area_id === area.id;
+    .then((areaData) => {
+        areas = areaData;
+        attractions.forEach(function() {
+            for (let i = 0; i < attractions.length; i++) {
+                let myArea = areas.filter((area) => {
+                    return attractions[i].area_id === area.id;
+                });
+                attractions[i].areaName = myArea[0].name;
+            }
         });
-        attractions[i].areaName = myArea[0].name;
-      }
-    });
-    return themepark.fbData.getAttrTypes();
-  })
-.then((typeData) => {
-    types = typeData;
-    attractions.forEach(function (){
-      for(let i = 0; i < attractions.length; i++) {
-        let myType = types.filter((type) => {
-          return attractions[i].type_id === type.id;
+        return themepark.fbData.getAttrTypes();
+    })
+    .then((typeData) => {
+        types = typeData;
+        attractions.forEach(function() {
+            for (let i = 0; i < attractions.length; i++) {
+                let myType = types.filter((type) => {
+                    return attractions[i].type_id === type.id;
+                });
+                attractions[i].typeName = myType[0].name;
+            }
         });
-        attractions[i].typeName = myType[0].name;
-      }
+        console.log("final attractions array", attractions);
     });
-    console.log("final attractions array", attractions);
-  });
