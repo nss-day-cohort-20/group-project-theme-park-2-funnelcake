d2b3e6b98762dfc570b125d18ab613932050f68d
Adobe-Android <tngamerboy@gmail.com>
2017-07-13 09:54:59 -0500

    Added some extra changes I made to fix the Boostrap-datetime config

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,5 @@
 'use strict';
 
-let $ = require('jquery');
 let fb = require('./fetch-fb');
 let themepark = {
     fbData: require('./fetch-fb')
@@ -29,33 +28,10 @@ $(".area").click(function() {
 
 
 // Bootstap datetimepicker
-$('#time').bootstrapMaterialDatePicker({date: false, shortTime: false, format: 'HH:mm'});
-$.material.init();
-// 
-// 
-// 
-// 
-// $(document).ready(function() {
-//   $('#time').bootstrapMaterialDatePicker({
-//     date: false,
-//     shortTime: false,
-//     format: 'HH:mm'});
-//   $.material.init();
-// }); 
-//     $(function() {
-//         $('#datetimepicker6').datetimepicker();
-//         $('#datetimepicker7').datetimepicker({
-//             useCurrent: false //Important! See issue #1075
-//         });
-//         $("#datetimepicker6").on("dp.change", function(e) {
-//             $('#datetimepicker7').data("DateTimePicker").minDate(e.date);
-//         });
-//         $("#datetimepicker7").on("dp.change", function(e) {
-//             $('#datetimepicker6').data("DateTimePicker").maxDate(e.date);
-//         });
-//     });
-    // $.material.init();
-// });
+
+$(document).ready(function() {
+$('#time').bootstrapMaterialDatePicker({date: false});
+});
 
 themepark.fbData.getAttr()
     .then((attrData) => {
