ff537cc03befe80c2e700171647673c7ba3b73ca
David Regan <dregan1@comcast.net>
2017-07-13 23:06:50 -0500

    finished separating functions and modularizing

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,9 @@
 'use strict';
-// let moment = "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.js";
+
 let fb = require('./fetch-fb');
 
-let themepark = {
-    fbData: require('./fetch-fb')
-};
-// let builder = {domInter: require('./DOMInteraction')};
+let themepark = {fbData: require('./fetch-fb')};
+let builder = {domInter: require('./DOMInteraction')};
 
 let attractions = [];
 let areas = [];
@@ -16,65 +14,18 @@ let textInput = document.getElementById('textInput');
 let submitBtn = document.getElementById('submitBtn');
 
 submitBtn.addEventListener("click", function() {
-  submitPrint($(this));
+  builder.domInter.submitPrint($(this), attractions);
 });
 
 textInput.addEventListener('keypress', function() {
   if (textInput.value !== '' && event.key === 'Enter') {
-    submitPrint($(this));
+    builder.domInter.submitPrint($(this), attractions);
   }
 }); 
 
 $(".area").click(function() {
-  print($(this));
+  builder.domInter.print($(this), attractions);
 });
-
-
-function clear (area) {
-  $('.area.highlight').not(area).removeClass('highlight');
-    $(".side-bar").empty();
-}
-
-function details (div) {
-    $(".attDetails").hide();
-    $(div).next().show();
-}
-
-//compares user input to attraction name, finds the area id and highlights the appropriate area
-function submitPrint (area) {
-  clear(area);
-  let userInput = textInput.value.toLowerCase();
-  clear(area);
-  $.each(attractions, function (name, value) {
-    let lowerName = value.name.toLowerCase();
-    if (userInput == lowerName) {
-      $("#" + value.area_id).toggleClass("highlight");
-     $(".side-bar").append(`<p class="visibility"><a href="#">${value.name}</a> (${value.typeName})</p>
-        <div class="attDetails" style="display: none">${value.description}<br><p class="times">Times: ${value.times}</p></div>`);
-    }
-  });
-     $(".visibility").click(function(){
-      details($(this));
-  });
-  textInput.value = '';
-}
-
-//selecting the area boxes, loop through attractions to match area id with area clicked on and print that attraction to the dom
-//click even embedded to allow clicking on attraction name and making the description visible
-function print (area) {
-  clear(area);
-  area.toggleClass("highlight");
-  let currentId = area.attr("id"); 
-  $.each(attractions, function (name, value) {
-    if (currentId == value.area_id) { 
-     $(".side-bar").append(`<p class="visibility"><a href="#">${value.name}</a> (${value.typeName})</p>
-        <div class="attDetails" style="display: none">${value.description}<br><p class="times">Times: ${value.times}</p></div>`);
-    }
-  });
-     $(".visibility").click(function(){
-      details($(this));
-  });
-}
 
 themepark.fbData.getAttr()
     .then((attrData) => {
@@ -123,22 +74,20 @@ themepark.fbData.getAttr()
               $.each(attractions, function (name, array) {
                 $.each(array.times, function (name, value) {
                   if (timeVal == value) {
-                    $(".side-bar").append(`<p class="visibility"><a href="#">${array.name}</a> (${array.typeName})</p>
-                    <div class="attDetails" style="display: none">${array.description}<br><p class="times">Times: ${array.times}</p></div>`);
+                    builder.domInter.atts(array);
                   }
                 });
               });
-
-      $(".visibility").click(function(){
-      details($(this));
-  });
+            $(".visibility").click(function(){
+              builder.domInter.details($(this));
+            });
 
             });
         });
     });
 
- function timeCheck(val) {
-  if (val === undefined) {
-    $(".times").hide();
-  }
-}
+//  function timeCheck(val) {
+//   if (val === undefined) {
+//     $(".times").hide();
+//   }
+// }
